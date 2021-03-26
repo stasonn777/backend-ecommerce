35ee723c2b282dcee658b5728d0111f2174af372
stasonn777 <stasonn777@gmail.com>
2021-03-26 18:24:15 +0200

    POST user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+const { body, validationResult } = require('express-validator');
+
+const User = require('../models/User')
 
 // @route       GET api/users
 // @desc        Get all users
@@ -11,8 +15,37 @@ router.get('/', (req, res) => {
 // @route       POST api/users
 // @desc        Create users 
 // @access      Public
-router.post('/', (req, res) => {
-  res.send('Create new user')
+router.post('/', [
+  body('name', 'Name is required').not().isEmpty(),
+  body('email', 'Please include valid email').isEmail(),
+  body('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+], 
+async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
+
+  const {name, email, password, role} = req.body
+
+  try {
+    let user = await User.findOne({email}) // Check if user exists
+    if (user) {
+      return res.status(400).json({msg: 'User allready exists'})
+    }
+
+    user = new User({name, email, password, role})
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(password, salt) // Hash version of password
+    await user.save()
+    res.send('User saved')
+
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+
+  res.send('passed')
 })
 
 // @route       PUT api/users/:id
